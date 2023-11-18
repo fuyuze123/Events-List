5f85332f8fbad738647a1135f05c857643c6aae2
fuyuze123 <112993052+fuyuze123@users.noreply.github.com>
2023-11-18 23:24:16 +0000

    header file

--- header/event.h
+++ header/event.h
@@ -1,6 +1,40 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <string>
+
+
+class EVENT 
+{
+protected:
+    string date;
+    string time; 
+    string location;
+    string purpose;
+
+public:
+
+    EVENT(string userDate,string userTime,string userLocation,string userPurpose);
+    const string get_date();
+    const string get_time();
+    const string get_location();
+    const string get_purpose();
+
+    void set_date(string userDate);
+    void set_time(string userTime);
+    void set_location(string userLocation);
+    void set_purpose(string userPurpose)
+
+
+
+
+
+
+
+
+
+}
+
 
 
 
