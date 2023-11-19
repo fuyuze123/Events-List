026b6312265de2eddc9a2a265f4d3682150cd53a
fuyuze123 <112993052+fuyuze123@users.noreply.github.com>
2023-11-19 00:44:37 +0000

    Added main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "../header/event.h"
 #include "../header/collection.h"
+#include <iostream>
 
 int main()
 {
+    EVENT object("a","b","c","d");
+ 
 
-    
+    collection objectTest;
+    objectTest.addEvent(object);
+    objectTest.printList();
+    objectTest.ListToHTML();
 
 
 }
