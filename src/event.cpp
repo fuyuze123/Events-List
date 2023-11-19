397d844057efd970f66d5f13bc2f092c7e1c6a2f
Henry Lo <100541409+NoMercy678@users.noreply.github.com>
2023-11-18 17:46:55 -0800

    Updated Version with name variable

--- src/event.cpp
+++ src/event.cpp
@@ -1,7 +1,8 @@
 #include "../header/event.h"
 
- EVENT::EVENT(string userDate,string userTime,string userLocation,string userPurpose)
+ EVENT::EVENT(string userDate,string userTime,string userLocation,string userPurpose, string eventName)
  {
+    set_name(eventName);
     set_date(userDate);
     set_location(userLocation);
     set_time(userTime);
@@ -9,11 +10,13 @@
 
  }
 
+    const string EVENT::get_name(){return this->eventName;}
     const string EVENT::get_date(){return this->date;}
     const string EVENT::get_time(){return this->time;}
     const string EVENT::get_location(){return this->location;}
     const string EVENT::get_purpose(){return this->purpose;}
 
+    void EVENT::set_name(string eventName){this->eventName = eventName;}
     void EVENT::set_date(string userDate){this->date = userDate;}
     void EVENT::set_time(string userTime){this->time = userTime;}
     void EVENT::set_location(string userLocation){this->location = userLocation;}
