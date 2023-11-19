3f18634c5f9f32a512fea69cc15f7605d16a3a09
Henry Lo <100541409+NoMercy678@users.noreply.github.com>
2023-11-18 16:23:41 -0800

    push

--- src/event.cpp
+++ src/event.cpp
@@ -1,2 +1,37 @@
 #include "../header/event.h"
 
+class EventManager{
+private:
+    vector<string> events;
+
+public:
+    void createEvent (string date, string time, string location, string purpose){
+        set_date();
+        set_time();
+        set_location();
+        set_purpose();
+        
+        events.push_back({date, time, location, purpose});
+        cout << "Event created." << endl;
+    }
+    
+    void set_date(string userDate){
+    cout << "Please choose the date for your event: ";
+    getline(cin, date);
+    }
+
+    void set_time(string userTime){
+    cout << "Please choose the time for your event: ";
+    getline(cin, time);
+    }
+
+    void set_location(string userLocation){
+    cout << "Please input the location of your event: ";
+    getline(cin, location);
+    }
+
+    void set_purpose(string userPurpose){
+    cout << "Please state the purpose of your event: ";
+    getline(cin, purpose);
+    }
+}
